0cc9efc3a894292df2bc921abaefd12673164855
Eric Renfro <psi-jack@linux-help.org>
2024-09-06 22:59:27 -0400

    Updated with successful build of SystemRecue-Backup CD

--- src/functions.sh
+++ src/functions.sh
@@ -15,17 +15,6 @@ scriptPath="$(dirname "$(readlink -f "$0")")"
 configPath="$(readlink -f "${scriptPath}/../config")"
 debug=false
 
-if [[ ! -r "${configPath}/srb.cfg" ]]; then
-    # Start searching for common configuration paths
-    if [[ -r "$HOME/.config/systemrescue-backup/srb.cfg" ]]; then
-        configPath="$HOME/.config/systemrescue-backup"
-    elif [[ -r "/etc/systemrescue-backup/srb.cfg" ]]; then
-        configPath="/etc/systemrescue-backup"
-    else
-        echoerr "Warning: No configuration file path found. Defaults will be used."
-    fi
-fi
-
 
 ###########################################################
 ### Utility & Common Functions
@@ -33,6 +22,23 @@ fi
 
 is_bin_in_path() {
     builtin type -P "$1" &>/dev/null
+}
+
+# Check if argument is number.
+is_num() {
+    [[ -n "$1" && "$1" -eq "$1" ]] 2>/dev/null
+}
+
+# Convert signal name to signal number.
+to_sig_num() {
+    if is_num "$1"; then
+      # Signal is already number.
+      kill -l "$1" >/dev/null # Check that signal number is valid.
+      echo    "$1"            # Return result.
+    else
+      # Convert to signal number.
+      kill -l "$1"
+    fi
 }
 
 echoreg() {
@@ -187,6 +193,79 @@ run_modules() {
     done
 }
 
+init_squashfs() {
+    truncate -s0 "${systemrescue_data_dir}/sysrescue/build_into_srm/.squashfs-pseudo"
+}
+
+add_to_squashfs() {
+    local input="$1"
+
+    if $debug; then
+        echo "Adding to SquashFS: $1"
+    fi
+    echo "$1" >> "${systemrescue_data_dir}/sysrescue/build_into_srm/.squashfs-pseudo"
+}
+
+add_path_to_squashfs() {
+    local path="$1"
+    local dir_perm="$2"
+    local file_perm="$3"
+    local file_owner="$4"
+    local file_group="$5"
+    local perm
+
+    [[ -z "$dir_perm" ]]   && dir_perm="755"
+    [[ -z "$file_perm" ]]  && file_perm="644"
+    [[ -z "$file_owner" ]] && file_owner="root"
+    [[ -z "$file_group" ]] && file_group="root"
+
+    while read -r file; do
+        if [[ -d "$file" ]]; then
+            [[ "$dir_perm" == "match" ]] && perm="$(stat -c "%a" "$file")" || perm="$dir_perm"
+            #add_to_squashfs "${file/${systemrescue_data_dir}\/sysrescue\/build_into_srm\///} m $dir_perm root root"
+        elif [[ -f "$file" ]]; then
+            [[ "$file_perm" == "match" ]] && perm="$(stat -c "%a" "$file")" || perm="$file_perm"
+            #add_to_squashfs "${file/${systemrescue_data_dir}\/sysrescue\/build_into_srm\///} m $file_perm root root"
+        fi
+        add_to_squashfs "${file/${systemrescue_data_dir}\/sysrescue\/build_into_srm\///} m $perm root root"
+    done < <(find "$path")
+}
+
+trap_add() {
+    local handler="${1:?Handler required}"
+    #local signal="${2:?Signal required}"
+    local signals=("${@:2}")
+    local hdls
+    local signal
+
+    if [[ "${#signals[@]}" -lt 1 ]]; then
+        echo_fail 1 "Signal required"
+    fi
+
+    if $debug; then
+        echo "====================================================="
+    fi
+
+    for signal in "${signals[@]}"; do
+        hdls="$( trap -p "${signal}" | cut -f2 -d \' )"
+        trap "${hdls}${hdls:+;}${handler}" "${signal}"
+        if $debug; then
+            echo "Adding Trap: $handler (${signal})"
+            echo "Old Traps: $hdls"
+        fi
+    done
+
+    if $debug; then
+        echo "Current Traps:"
+        trap -p "${signals[@]}"
+    fi
+
+    if $debug; then
+        echo "====================================================="
+    fi
+}
+
+
 ###########################################################
 ### BACKUP and RESTORE FUNCTIONS
 ###########################################################
@@ -243,7 +322,7 @@ restore_btrfs_subvolumes() {
     done < "${restoreDir}/btrfs.dump"
 }
 
-restore_btfs_mounts() {
+restore_btrfs_mounts() {
     local rootBase mountSource mountDest
 
     rootBase="$1"
@@ -257,3 +336,19 @@ restore_btfs_mounts() {
         mount "$mountSource" "${rootBase}/${mountDest}"
     done < "$restoreDir/mounts.dump"
 }
+
+
+###########################################################
+### Initialization
+###########################################################
+
+if [[ ! -r "${configPath}/srb.cfg" ]]; then
+    # Start searching for common configuration paths
+    if [[ -r "$HOME/.config/systemrescue-backup/srb.cfg" ]]; then
+        configPath="$HOME/.config/systemrescue-backup"
+    elif [[ -r "/etc/systemrescue-backup/srb.cfg" ]]; then
+        configPath="/etc/systemrescue-backup"
+    else
+        echoerr "Warning: No configuration file path found. Defaults will be used."
+    fi
+fi
